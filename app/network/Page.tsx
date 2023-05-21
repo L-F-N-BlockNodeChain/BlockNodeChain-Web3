df065da86dd5dfc06e7593fc5d7717d873cd83fd
Abin <abinbvinod@gmail.com>
2023-05-21 23:55:00 +0530

    web

--- app/network/Page.tsx
+++ app/network/Page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
+import React from "react"
+
 
 export default function network()  {
     return (
-      <>
+      <div>
            <h1>Network</h1>
-           <Link href="/"> Link to Home Page </Link>
-      </>
+            
+      </div>
       
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-import Image from 'next/image'
-import 'tailwindcss/tailwind.css'
-
-
+import Image from 'next/image';
+import React from 'react';
+import 'tailwindcss/tailwind.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main >
-      
+    <main>
+      {/* for the network button */}
+      <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 sm:bottom-20 sm:w-1/2">
+        <div className="grid gap-1 items-start justify-center">
+          <div className="relative group">
+            <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-700 to-white-100 to-purple-600 rounded-lg blur opacity-75 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt animate-pulse" style={{ animationDuration: '1s' }}></div>
+            <Link href="/network">
+              <button className="relative px-2 py-2 bg-black rounded-lg leading-none flex items-center">
+                <div className="absolute w-8 h-8 text-violet-400 rotate-10 animate-ping" style={{ animationDuration: '1s' }}>
+                  <Image src="/main.png" alt="Logo" width={70} height={70} />
+                </div>
+                <h1 className="font-mono text-base pl-6 text-violet-400 group-hover:text-gray-100 transition duration-500">
+                  Block Node Chain Network &rarr;
+                </h1>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* For the logo */}
+      <div className="flex justify-center">
+        <a className="fixed flex items-center top-7 left-5" href="https://www.linkedin.com/company/lfnbnc">
+          <Image src="/svg.svg" alt="logo" width={80} height={80} />
+        </a>
+        <div className="fixed flex top-6 left-5">
+          <Image src="/whitesvg.svg" alt="Logo" width={80} height={80} />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
